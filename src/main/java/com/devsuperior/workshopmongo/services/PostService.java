b1a339e68536d5948b6c91b715e1cf2f8704e88b
icaro-freitas <icaro_freitas2014@outlook.com>
2024-11-14 12:13:05 -0300

    GET find posts by title

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -20,17 +21,11 @@ public class PostService {
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
 	}
 
+	public Flux<PostDTO> findByTitle(String text) {
+		return repository.searchTitle(text).map(x -> new PostDTO(x));		
+	}
+
 	/*
-	 * 
-	 * @Transactional(readOnly = true) public PostDTO findById(String id) { Post
-	 * post = repository.findById(id).orElseThrow(() -> new
-	 * ResourceNotFoundException("Recurso não encontrado")); return new
-	 * PostDTO(post); }
-	 * 
-	 * public List<PostDTO> findByTitle(String text) { List<PostDTO> result =
-	 * repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-	 * return result; }
-	 * 
 	 * public List<PostDTO> fullSearch(String text, Instant minDate, Instant
 	 * maxDate) { maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
 	 * List<PostDTO> result = repository.fullSearch(text, minDate,
